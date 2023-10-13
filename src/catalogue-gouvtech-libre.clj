19d4ff4f890159b227c22055c1064ad5bc24c57d
Bastien <bzg@gnu.org>
2023-10-13 14:48:05 +0200

    src/catalogue-gouvtech-libre.clj: Optionally spit .md file

--- src/catalogue-gouvtech-libre.clj
+++ src/catalogue-gouvtech-libre.clj
@@ -4,6 +4,9 @@
 ;; SPDX-License-Identifier: EPL-2.0
 ;; License-Filename: LICENSE.txt
 
+(require '[babashka.cli :as cli])
+(def opts (cli/parse-opts *command-line-args*))
+
 (println "Fetching catalogue-gouvtech data from data.gouv.fr...")
 
 (def catalogue-gouvtech-url "https://www.data.gouv.fr/fr/datasets/r/184e607b-a45d-4ef2-8105-85967959b44b")
@@ -15,7 +18,8 @@
     (when (= (:status res) 200) (:body res))))
 
 (defn- rows->maps [csv]
-  (let [headers (map keyword (first csv))
+  (let [headers (map #(keyword (str/replace % " " "_"))
+                     (first csv))
         rows    (rest csv)]
     (map #(zipmap headers %) rows)))
 
@@ -25,10 +29,24 @@
     (catch Exception e
       (println (.getMessage e)))))
 
+(def catalogue-gouvtech-libre
+  (filter #(= (:Distribution %) "Logiciel libre")
+          (csv-url-to-map catalogue-gouvtech-url)))
+
 (spit "catalogue-gouvtech-libre.json"
-      (json/generate-string
-       (filter #(= (:Distribution %) "Logiciel libre")
-               (csv-url-to-map catalogue-gouvtech-url))
-       {:pretty true}))
+      (json/generate-string catalogue-gouvtech-libre {:pretty true}))
 
 (println "Wrote catalogue-gouvtech-libre.json in this directory.")
+
+(when (:md opts)
+  (spit "catalogue-gouvtech-libre.md" "")
+  (doall (map #(spit "catalogue-gouvtech-libre.md"
+                     (str "# " (:Nom_solution %) "\n\n"
+                          (:Description_courte %) "\n\n"
+                          "- **Licence**: " (:Licence_opensource %) "\n"
+                          "- **Lien GouvTech**: " (:Collection_ID %) "\n"
+                          "- **Lien SILL**: " (:Lien_SILL %) "\n"
+                          "- **Lien Wikidata**: ???" "\n\n")
+                     :append true)
+              catalogue-gouvtech-libre))
+  (println "Wrote catalogue-gouvtech-libre.md in this directory."))
