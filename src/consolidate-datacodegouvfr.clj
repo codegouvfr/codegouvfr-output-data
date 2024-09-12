1bff05df062127b77ee289c29a6dbbe7f5988a15
Bastien Guerry <bastien.guerry@data.gouv.fr>
2024-09-12 09:46:04 +0200

    src/consolidate-datacodegouvfr.clj: Handle ospo_url

--- src/consolidate-datacodegouvfr.clj
+++ src/consolidate-datacodegouvfr.clj
@@ -92,41 +92,43 @@
                        (not archived))))))
 
 (def owners-keys-mapping
-  {:id  :id
-   :r   :repositories_count
-   :o   :html_url
-   :au  :icon_url
-   :n   :name
-   :m   :ministry
-   :l   :login
-   :c   :created_at
-   :d   :description
-   :f   :floss_policy
-   :h   :website
-   :p   :forge
-   :e   :email
-   :a   :location
-   :pso :organization})
+  {:id :id
+   :r  :repositories_count
+   :o  :html_url
+   :au :icon_url
+   :n  :name
+   :m  :ministry
+   :l  :login
+   :c  :created_at
+   :d  :description
+   :f  :floss_policy
+   :os :ospo_url
+   :h  :website
+   :p  :forge
+   :e  :email
+   :a  :location
+   :ps :organization})
 
 (defn owners-to-output [owners]
   (for [[owner_id {:keys [description repositories_count html_url icon_url name
-                          pso_top_id_name login created_at floss_policy website
-                          forge email location pso]}] owners]
-    {:id  owner_id
-     :r   repositories_count
-     :o   html_url
-     :au  icon_url
-     :n   name
-     :m   pso_top_id_name
-     :l   login
-     :c   created_at
-     :d   (if (not-empty description) (subs description 0 (min (count description) 200)) "")
-     :f   floss_policy
-     :h   website
-     :p   forge
-     :e   email
-     :a   location
-     :pso pso}))
+                          pso_top_id_name login created_at floss_policy ospo_url
+                          website forge email location pso]}] owners]
+    {:id owner_id
+     :r  repositories_count
+     :o  html_url
+     :au icon_url
+     :n  name
+     :m  pso_top_id_name
+     :l  login
+     :c  created_at
+     :d  (if (not-empty description) (subs description 0 (min (count description) 200)) "")
+     :f  floss_policy
+     :h  website
+     :p  forge
+     :e  email
+     :a  location
+     :os ospo_url
+     :ps pso}))
 
 (defn owners-to-csv []
   (as-> @owners o
@@ -342,14 +344,22 @@
           (let [owner_url
                 (str/lower-case
                  (format (str (:hosts urls) "/%s/owners/%s") f group))
-                {:strs [pso pso_id floss_policy]} group-data]
+                {:strs [pso pso_id floss_policy ospo_url]} group-data]
             (swap! owners update-in [owner_url]
-                   #(assoc % :pso pso :pso_id pso_id :floss_policy floss_policy
+                   #(assoc %
+                           :pso pso
+                           :pso_id pso_id
+                           :floss_policy floss_policy
+                           :ospo_url ospo_url
                            :forge (get forge-data "forge")))))
         (doseq [[k _] (filter #(str/includes? (key %) (str/lower-case f)) @owners)]
-          (let [{:strs [pso pso_id floss_policy forge]} forge-data]
+          (let [{:strs [pso pso_id floss_policy ospo_url forge]} forge-data]
             (swap! owners update-in [k]
-                   #(assoc % :pso pso :pso_id pso_id :floss_policy floss_policy
+                   #(assoc %
+                           :pso pso
+                           :pso_id pso_id
+                           :floss_policy floss_policy
+                           :ospo_url ospo_url
                            :forge forge)))))))
   ;; Add top_id and top_id_name to owners
   (let [annuaire      (fetch-annuaire)
